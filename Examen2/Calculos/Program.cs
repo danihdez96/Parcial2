19dba41439e5e4d7d7f807f52a85731232b2e1f8
danihdez96 <47672374+danihdez96@users.noreply.github.com>
2019-03-19 19:43:16 -0600

    Add files via upload

---
+++ Examen2/Calculos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace Calculos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Class1 obj1 = new Class1();
+            obj1.Operaciones1();
+
+            Class1 obj2 = new Class1();
+            obj2.Division();
+
+
+            Class1 obj3 = new Class1();
+            obj3.ImpNombres();*/
+
+            Class1 obj4 = new Class1();
+            obj4.ImpEdades();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Examen2/ClassLibrary1/Class1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int opc, num1, num2, resul;
+
+        public void Operaciones1()
+        {
+            Console.WriteLine("Presione 1 para multiplicar los numeros");
+            opc = int.Parse(Console.ReadLine());
+            switch (opc)
+            {
+                case 1:
+                    Console.WriteLine("Ingrese un numero entero cualquiera");
+                    num1 = int.Parse(Console.ReadLine());
+                    if (num1 != 0)
+                    {
+                        Console.WriteLine("Ingrese otro numero entero");
+                        num2 = int.Parse(Console.ReadLine());
+                        resul = num1 * num2;
+                        Console.WriteLine("El resultado de la multiplicacion es: " + resul);
+                    }
+                    else
+                    {
+                        Console.WriteLine("El producto de 0 por cualquier otro numero es 0");
+                    }
+                    break;
+            }
+        }
+
+
+        public int num3, num4, opc1;
+        public double resul1;
+        public void Division()
+        {
+            Console.WriteLine("Presione 2 para dividir los numeros");
+            opc1 = int.Parse(Console.ReadLine());
+            switch (opc1)
+            {
+                case 2:
+                    Console.WriteLine("Ingrese un numero entero cualquiera");
+                    num3 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese un otro numero entero cualquiera");
+                    num4 = int.Parse(Console.ReadLine());
+                    if (num3 !=0 && num4 != 0)
+                    {
+                        resul1 = num3 / num4;
+                        Console.WriteLine("El resultado de la multiplicacion es: " + resul1);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: No se puede dividir entre cero");
+                    }
+                    break;
+            }
+
+        }
+
+
+        string nom;
+
+        public void ImpNombres()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Digite los nombres de los alumnos");
+                nom = Console.ReadLine();
+                Console.WriteLine(nom);
+            }
+        }
+
+
+        int edad, c1=0;
+
+        public void ImpEdades()
+        {
+            do
+            {
+                Console.WriteLine("Digite 5 edades");
+                edad = int.Parse(Console.ReadLine());
+                Console.WriteLine(edad);
+            } while (c1 <= 5);
+        }
+    }
+}
